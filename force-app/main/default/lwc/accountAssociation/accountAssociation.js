bab38b38e33b999b00b4d120dde7a32fe217dd41
Avishai Dotan <108017307+AvishaiDotan@users.noreply.github.com>
2024-03-10 00:08:33 +0200

    fully implement lookup connection backend

--- force-app/main/default/lwc/accountAssociation/accountAssociation.js
+++ force-app/main/default/lwc/accountAssociation/accountAssociation.js
@@ -7,6 +7,7 @@ import AssociateContactToAccount from '@salesforce/apex/ContactController.Associ
 import ACCOUNT_PHONE_FIELD from '@salesforce/schema/Account.Phone';
 import ACCOUNT_TYPE_FIELD from '@salesforce/schema/Account.Type';
 import ACCOUNT_DESCRIPTION_FIELD from '@salesforce/schema/Account.Description';
+import ACCOUNT_NAME_FIELD from '@salesforce/schema/Account.Name';
 
 export default class AccountAssociation extends LightningElement {
     @api recordId;
@@ -17,12 +18,15 @@ export default class AccountAssociation extends LightningElement {
         description: ''
     };
 
+    accountName;
+
     @track nonAssociatedContacts;
 
-    @wire(getRecord, { recordId: '$recordId', fields: [ACCOUNT_PHONE_FIELD, ACCOUNT_TYPE_FIELD, ACCOUNT_DESCRIPTION_FIELD] })
+    @wire(getRecord, { recordId: '$recordId', fields: [ACCOUNT_NAME_FIELD, ACCOUNT_PHONE_FIELD, ACCOUNT_TYPE_FIELD, ACCOUNT_DESCRIPTION_FIELD] })
     wiredRecord({ error, data }) {
         if (data) {
             this.setFormattedAccountDetails(data);
+            this.accountName = getFieldValue(data, ACCOUNT_NAME_FIELD);
         } else if (error) {
             console.error('Error fetching account data:', error);
         }
@@ -30,7 +34,7 @@ export default class AccountAssociation extends LightningElement {
     };
 
     @wire(GetAllNonAssociatedContacts)
-    wiredRecord({ error, data }) {
+    wiredAssociatedContacts({ error, data }) {
         if (data) {
             console.log('GetAllNonAssociatedContacts:', data);
             this.nonAssociatedContacts = data;
@@ -42,8 +46,9 @@ export default class AccountAssociation extends LightningElement {
 
     async onAssociateContact(event) {
         try {
-            console.log('onAssociateContact:', event.detail.id, this.recordId);
-            await AssociateContactToAccount({ accountId: this.recordId, contactId: event.detail.id });
+            console.log('event.detail.contactId:', event.detail.contactId);
+            console.log('this.accountName:', this.accountName);
+            await AssociateContactToAccount({ accountId: this.accountName, contactId: event.detail.contactId });
         }
         catch (e) {
             console.log(e);
--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,11 +1,11 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AccountList extends LightningElement {
-    @api nonAssociatedContacts;
+    @api nonAssociatedContacts = [];
 
     onAssociateContact(event) {
         const contactId = event.target.dataset.id;
-        const messageEvent = new CustomEvent('associatecontact', { detail: contactId });
+        const messageEvent = new CustomEvent('associatecontact', { detail: {contactId} });
         this.dispatchEvent(messageEvent);
     }
 }
